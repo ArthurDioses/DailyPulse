fe11b53e7c20d932390932f508db3111aa80ae7b
Arthur Dioses <adioses93@gmail.com>
2024-03-20 17:55:11 -0500

    Impl() - Articles Android Screen with Compose

--- androidApp/src/main/java/com/dioses/dailypulse/android/screens/AboutScreen.kt
+++ androidApp/src/main/java/com/dioses/dailypulse/android/screens/AboutScreen.kt
@@ -1,4 +1,4 @@
-package com.dioses.dailypulse.android
+package com.dioses.dailypulse.android.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
