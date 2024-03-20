4e442a7c236d792dd82a2faabf68af0c97d9b857
Arthur Dioses <adioses93@gmail.com>
2024-03-20 13:27:06 -0500

    Impl() - Articles View Model part 2

--- shared/src/commonMain/kotlin/com/dioses/dailypulse/articles/ArticlesViewModel.kt
+++ shared/src/commonMain/kotlin/com/dioses/dailypulse/articles/ArticlesViewModel.kt
@@ -1,8 +1,10 @@
 package com.dioses.dailypulse.articles
 
 import com.dioses.dailypulse.BaseViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 /****
  * Project: DailyPulse
@@ -13,4 +15,15 @@ import kotlinx.coroutines.flow.StateFlow
 class ArticlesViewModel : BaseViewModel() {
     private val _articlesState: MutableStateFlow<ArticlesState> = MutableStateFlow(ArticlesState())
     val articlesState: StateFlow<ArticlesState> get() = _articlesState
+
+    init {
+        getArticles()
+    }
+
+    private fun getArticles() {
+        scope.launch {
+            delay(500)
+            _articlesState.emit(ArticlesState())
+        }
+    }
 }
