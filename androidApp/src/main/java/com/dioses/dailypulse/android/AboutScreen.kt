7d9838c9fb11b3191ce63ad9b6567b7312edbd0e
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2024-03-19 23:05:03 -0500

    Impl() - Android UI with Jetpack Compose

---
+++ androidApp/src/main/java/com/dioses/dailypulse/android/AboutScreen.kt
@@ -0,0 +1,79 @@
+package com.dioses.dailypulse.android
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.dioses.dailypulse.Platform
+
+/****
+ * Project: DailyPulse
+ * From: com.dioses.dailypulse.android
+ * Created by Arthur Dioses Reto on 19/03/24 at 10:42 PM
+ * All rights reserved 2024.
+ ****/
+@Composable
+fun AboutScreen() {
+    Column {
+        Toolbar()
+        ContentView()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Toolbar() {
+    TopAppBar(title = { Text(text = "About Device") })
+}
+
+@Composable
+fun ContentView() {
+
+    val items = makeItems()
+
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(items) { row ->
+            RowView(title = row.first, subTitle = row.second)
+        }
+    }
+
+}
+
+private fun makeItems(): List<Pair<String, String>> {
+    val platform = Platform()
+    platform.logSystemInfo()
+
+    return listOf(
+        Pair("Operating System", "${platform.osName} ${platform.osVersion}"),
+        Pair("Device", platform.deviceModel),
+        Pair("Density", platform.density.toString())
+    )
+}
+
+
+@Composable
+fun RowView(title: String, subTitle: String) {
+    Column(modifier = Modifier.padding(8.dp)) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.bodySmall,
+            color = Color.Gray
+        )
+        Text(
+            text = subTitle,
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+
+    Divider()
+}
