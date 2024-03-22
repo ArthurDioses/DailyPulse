120dc20d31f2915b0d0f3e57ea713c929aa5f2a0
Arthur Dioses <adioses93@gmail.com>
2024-03-21 23:11:47 -0500

    Impl() - Parsing the backend response

---
+++ shared/src/commonMain/kotlin/com/dioses/dailypulse/articles/ArticlesResponse.kt
@@ -0,0 +1,18 @@
+package com.dioses.dailypulse.articles
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/****
+ * Project: DailyPulse
+ * From: com.dioses.dailypulse.articles
+ * Created by Arthur Dioses Reto on 21/03/24 at 10:37 PM
+ * All rights reserved 2024.
+ ****/
+
+@Serializable
+data class ArticlesResponse(
+    @SerialName("status") val status: String,
+    @SerialName("totalResults") val results: Int,
+    @SerialName("articles") val articles: List<ArticleRaw>,
+)
