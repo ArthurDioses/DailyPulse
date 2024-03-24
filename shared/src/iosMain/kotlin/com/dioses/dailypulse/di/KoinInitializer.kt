11cfdd8dc75457a78aa35686640b514012006bdf
Arthur Dioses <adioses93@gmail.com>
2024-03-24 14:32:29 -0500

    Impl() - Initialize Koin in IOS

---
+++ shared/src/iosMain/kotlin/com/dioses/dailypulse/di/KoinInitializer.kt
@@ -0,0 +1,25 @@
+package com.dioses.dailypulse.di
+
+import com.dioses.dailypulse.articles.ArticlesViewModel
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import org.koin.core.context.startKoin
+
+/****
+ * Project: DailyPulse
+ * From: com.dioses.dailypulse.di
+ * Created by Arthur Dioses Reto on 24/03/24 at 2:26 PM
+ * All rights reserved 2024.
+ ****/
+
+fun initKoin() {
+    val module = sharedKoinModule
+
+    startKoin {
+        modules(module)
+    }
+}
+
+class ArticlesInjector : KoinComponent {
+    val articlesViewModel: ArticlesViewModel by inject()
+}
