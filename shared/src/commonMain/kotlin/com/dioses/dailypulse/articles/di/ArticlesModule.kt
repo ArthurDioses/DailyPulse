c860756af11f97ad940fe017ef0a3a3bf55684ec
Arthur Dioses <adioses93@gmail.com>
2024-03-24 12:38:53 -0500

    Impl() - Articles Koin Module

---
+++ shared/src/commonMain/kotlin/com/dioses/dailypulse/articles/di/ArticlesModule.kt
@@ -0,0 +1,19 @@
+package com.dioses.dailypulse.articles.di
+
+import com.dioses.dailypulse.articles.ArticlesService
+import com.dioses.dailypulse.articles.ArticlesUseCase
+import com.dioses.dailypulse.articles.ArticlesViewModel
+import org.koin.dsl.module
+
+/****
+ * Project: DailyPulse
+ * From: com.dioses.dailypulse.articles.di
+ * Created by Arthur Dioses Reto on 24/03/24 at 11:33 AM
+ * All rights reserved 2024.
+ ****/
+
+val articlesModule = module {
+    single<ArticlesService> { ArticlesService(get()) }
+    single<ArticlesUseCase> { ArticlesUseCase(get()) }
+    single<ArticlesViewModel> { ArticlesViewModel() }
+}
