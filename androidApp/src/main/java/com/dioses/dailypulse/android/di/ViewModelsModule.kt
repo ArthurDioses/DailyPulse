85bacf3f2422a4172c81afc13a2683aae554303e
Arthur Dioses <adioses93@gmail.com>
2024-03-24 13:18:06 -0500

    Impl() - Completing the dependency graph

---
+++ androidApp/src/main/java/com/dioses/dailypulse/android/di/ViewModelsModule.kt
@@ -0,0 +1,18 @@
+package com.dioses.dailypulse.android.di
+
+import com.dioses.dailypulse.articles.ArticlesViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+/****
+ * Project: DailyPulse
+ * From: com.dioses.dailypulse.android.di
+ * Created by Arthur Dioses Reto on 24/03/24 at 1:03 PM
+ * All rights reserved 2024.
+ ****/
+
+val viewModelsModule = module {
+    viewModel {
+        ArticlesViewModel(get())
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/dioses/dailypulse/di/SharedKoinModules.kt
@@ -0,0 +1,15 @@
+package com.dioses.dailypulse.di
+
+import com.dioses.dailypulse.articles.di.articlesModule
+
+/****
+ * Project: DailyPulse
+ * From: com.dioses.dailypulse.di
+ * Created by Arthur Dioses Reto on 24/03/24 at 12:55 PM
+ * All rights reserved 2024.
+ ****/
+
+val sharedKoinModule = listOf(
+    articlesModule,
+    networkModule
+)
