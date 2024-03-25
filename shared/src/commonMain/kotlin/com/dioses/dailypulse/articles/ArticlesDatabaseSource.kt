e8d41f90524a9ede5afe4c5d2b0f013b3a8d48b8
Arthur Dioses <adioses93@gmail.com>
2024-03-24 20:15:46 -0500

    Impl() - Articles DatabaseSource part 2

--- shared/src/commonMain/kotlin/com/dioses/dailypulse/articles/ArticlesDatabaseSource.kt
+++ shared/src/commonMain/kotlin/com/dioses/dailypulse/articles/ArticlesDatabaseSource.kt
@@ -14,6 +14,26 @@ class ArticlesDatabaseSource(private val database: DailyPulseDatabase) {
     fun getAllArticles(): List<ArticleRaw> =
         database.dailyPulseDatabaseQueries.selectAllArticles(::mapToArticleRaw).executeAsList()
 
+    fun insertArticles(articles: List<ArticleRaw>) {
+        database.dailyPulseDatabaseQueries.transaction {
+            articles.forEach { articleRaw ->
+                insertArticles(articleRaw)
+            }
+        }
+    }
+
+    fun clearArticles() =
+        database.dailyPulseDatabaseQueries.removeAllArticles()
+
+    private fun insertArticles(articleRaw: ArticleRaw) {
+        database.dailyPulseDatabaseQueries.insertArticle(
+            articleRaw.title,
+            articleRaw.desc,
+            articleRaw.date,
+            articleRaw.imageUrl
+        )
+    }
+
     private fun mapToArticleRaw(
         title: String,
         desc: String?,
