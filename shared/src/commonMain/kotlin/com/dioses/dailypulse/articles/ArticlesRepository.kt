c2535b242a15619fe786259eeff0d2f4da364b48
Arthur Dioses <adioses93@gmail.com>
2024-03-24 20:20:34 -0500

    Impl() - Articles Repository

---
+++ shared/src/commonMain/kotlin/com/dioses/dailypulse/articles/ArticlesRepository.kt
@@ -0,0 +1,23 @@
+package com.dioses.dailypulse.articles
+
+/****
+ * Project: DailyPulse
+ * From: com.dioses.dailypulse.articles
+ * Created by Arthur Dioses Reto on 24/03/24 at 8:16 PM
+ * All rights reserved 2024.
+ ****/
+class ArticlesRepository(
+    private val dataSource: ArticlesDatabaseSource,
+    private val service: ArticlesService,
+) {
+    suspend fun getArticles(): List<ArticleRaw> {
+        val articlesDb = dataSource.getAllArticles()
+
+        if (articlesDb.isEmpty()) {
+            val fetchedArticles = service.fetchArticles()
+            dataSource.insertArticles(fetchedArticles)
+            return fetchedArticles
+        }
+        return articlesDb
+    }
+}
