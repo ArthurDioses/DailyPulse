7b5a20e219abcbde6eb4f6abdf61533226a54ad8
Arthur Dioses <adioses93@gmail.com>
2024-03-24 23:27:03 -0500

    Impl() - Testing the Android pull to refresh

--- shared/src/commonMain/kotlin/com/dioses/dailypulse/articles/ArticlesViewModel.kt
+++ shared/src/commonMain/kotlin/com/dioses/dailypulse/articles/ArticlesViewModel.kt
@@ -30,6 +30,13 @@ class ArticlesViewModel(
 
     fun getArticles(forceFetch: Boolean = false) {
         scope.launch {
+            _articlesState.emit(
+                ArticlesState(
+                    loading = true,
+                    articles = _articlesState.value.articles
+                )
+            )
+
             val fetchedArticles = useCase.getArticles(forceFetch)
 
             _articlesState.emit(ArticlesState(articles = fetchedArticles))
